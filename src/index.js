8b012796315b7a2b8c7cb504b3eb97a8d3199dcc
Eric Mugo <mugoericpg20@gmail.com>
2023-06-08 21:19:46 +0300

    Handle change

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import React, {useEffect, useState} from 'react';
+
+const WeatherApp = () => {
+  const [cityValue, setCityValue] = useState('');
+  const [weatherData, setWeatherData] = useState(null);
+
+  const key = "44fd0dcc0901a5fc29bd759611208d3b";
+  
+  const handleInputChange = (event) => {
+    setCityValue(event.target.value);
+  };
