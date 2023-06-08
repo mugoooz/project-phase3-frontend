a9e631f8ed3093ac2543db71e4116be8e0242c32
Eric Mugo <mugoericpg20@gmail.com>
2023-06-08 21:42:51 +0300

    Added functionality

--- src/index.js
+++ src/index.js
@@ -1,81 +1,75 @@
 import React, { useEffect, useState } from 'react';
+import 'src/index.css'; 
+import key from 'src/key.js'; 
 
 const WeatherApp = () => {
-  const [cityValue, setCityValue] = useState('');
-  const [weatherData, setWeatherData] = useState(null);
-
-  const key = "44fd0dcc0901a5fc29bd759611208d3b";
-  
-  const handleInputChange = (event) => {
-    setCityValue(event.target.value);
-  };
-
-  const getWeather = () => {
-    if (cityValue.length === 0) {
-      // Display error message if city name is empty
-      setWeatherData(null);
-      return;
-    }
-
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${key}&units=metric`;
-
-    // Clear the input field
-    setCityValue('');
-
-    fetch(url)
-      .then((resp) => resp.json())
-      .then((data) => {
-        setWeatherData(data);
-      })
-      .catch(() => {
-        // Display error message if city not found
-        setWeatherData(null);
-      });
-  };
+  const [cityValue, setCityValue] = useState("Nairobi");
+  const [result, setResult] = useState("");
 
   useEffect(() => {
-    // Fetch weather data on page load
     getWeather();
   }, []);
 
-  return (
-    <div>
-      <input
-        type="text"
-        id="city"
-        value={cityValue}
-        onChange={handleInputChange}
-      />
-      <button id="search-btn" onClick={getWeather}>
-        Search
-      </button>
-      <div id="result">
-        {weatherData ? (
-          <div>
-            <h2>{weatherData.name}</h2>
-            <h4 className="weather">{weatherData.weather[0].main}</h4>
-            <h4 className="desc">{weatherData.weather[0].description}</h4>
-            <img
-              src={`https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`}
-              alt={weatherData.weather[0].main}
-            />
-            <h1>{weatherData.main.temp} &#176;</h1>
-            <div className="temp-container">
+  const getWeather = () => {
+    if (cityValue.length === 0) {
+      setResult(`<h3 class="msg">Please enter a city name</h3>`);
+    } else {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${key}&units=metric`;
+
+      setCityValue("");
+      fetch(url)
+        .then((resp) => resp.json())
+        .then((data) => {
+          console.log(data);
+          console.log(data.weather[0].icon);
+          console.log(data.weather[0].main);
+          console.log(data.weather[0].description);
+          console.log(data.name);
+          console.log(data.main.temp_min);
+          console.log(data.main.temp_max);
+
+          setResult(`
+          <h2>${data.name}</h2>
+          <h4 class="weather">${data.weather[0].main}</h4>
+          <h4 class="desc">${data.weather[0].description}</h4>
+          <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png">
+          <h1>${data.main.temp} &#176;</h1>
+          <div class="temp-container">
               <div>
-                <h4 className="title">min</h4>
-                <h4 className="temp">{weatherData.main.temp_min}&#176;</h4>
+                  <h4 class="title">min</h4>
+                  <h4 class="temp">${data.main.temp_min}&#176;</h4>
               </div>
               <div>
-                <h4 className="title">max</h4>
-                <h4 className="temp">{weatherData.main.temp_max}&#176;</h4>
+                  <h4 class="title">max</h4>
+                  <h4 class="temp">${data.main.temp_max}&#176;</h4>
               </div>
-            </div>
           </div>
-        ) : (
-          <h3 className="msg">
-            {cityValue.length === 0 ? 'Please enter a city name' : 'City not found'}
-          </h3>
-        )}
+          `);
+        })
+        .catch(() => {
+          setResult(`<h3 class="msg">City not found</h3>`);
+        });
+    }
+  };
+
+  return (
+    <div className="wrapper">
+      <div className="shape shape-1"></div>
+      <div className="shape shape-2"></div>
+      <div className="container">
+        <div className="search-container">
+          <input
+            type="text"
+            placeholder="Enter a city name"
+            id="city"
+            value={cityValue}
+            onChange={(e) => setCityValue(e.target.value)}
+          />
+          <button id="search-btn" onClick={getWeather}>
+            Search
+          </button>
+        </div>
+        <div id="result" dangerouslySetInnerHTML={{ __html: result }}></div>
       </div>
     </div>
   );
---
+++ src/key.js
@@ -0,0 +1,3 @@
+export const  key = "44fd0dcc0901a5fc29bd759611208d3b";
+
+
