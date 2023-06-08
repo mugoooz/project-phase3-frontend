6b9f38ceb1edcddb779de9a875c6f8726a91e15a
Eric Mugo <mugoericpg20@gmail.com>
2023-06-08 22:03:22 +0300

    set city

--- src/App.js
+++ src/App.js
@@ -1,1 +1,15 @@
 import React, { useEffect, useState } from 'react';
+
+const App = () => {
+  const [city, setCity] = useState('Nairobi');
+  const [result, setResult] = useState('');
+
+  const getWeather = () => {
+    let cityValue = city;
+
+    if (cityValue.length === 0) {
+      setResult(<h3 className="msg">Please enter a city name</h3>);
+    } else {
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${key}&units=metric`;
+
+      setCity('');
